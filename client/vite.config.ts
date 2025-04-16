4fb9012536259da75efc09d7fb51f346be47bd30
DonavonCai <donavoncai1998@gmail.com>
2025-04-16 17:03:33 -0500

    Scaffold templating

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/static/",
   plugins: [react()],
   build: {
+    manifest: "manifest.json",
     emptyOutDir: true,
-    outDir: "../server/bundles",
-    assetsDir: "static"
-  }
+    outDir: "../server/assets",
+  },
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'http://my-backend.example.com',
+    },
+  },
 })
 
