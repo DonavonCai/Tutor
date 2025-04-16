556c413a66bfd1b9f5034f26b23f8e9a42b236c3
DonavonCai <donavoncai1998@gmail.com>
2025-04-16 17:03:33 -0500

    Scaffold templating

--- server/webpage/views.py
+++ server/webpage/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"})
